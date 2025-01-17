41372ee241a7ea00aaca0336ab011a6abd315fea
Filipe Pedro <aeropedro@yahoo.com>
2025-01-17 15:49:44 +0100

    updated tickers and plot size

--- generate_json.py
+++ generate_json.py
@@ -12,28 +12,35 @@ import os
 load_dotenv()
 api_key = os.getenv("COINGECKO_API_KEY")
 
-charts_path = './images/crypto_charts/'
-days = '60'
+charts_path = './crypto_charts/'
+days = '90'
 
 tickers_to_watch = {
     "bitcoin": {"token": "BTC", "name": "Bitcoin (BTC)", "color": "orange"},
     "ethereum": {"token": "ETH", "name": "Ethereum (ETH)", "color": "gray"},
-    "axie-infinity": {"token": "AXS", "name": "Axie (AXS)", "color": "darkblue"},
-    "smooth-love-potion": {"token": "SLP", "name": "SLP", "color": "hotpink"},
+    "axie-infinity": {"token": "AXS", "name": "Axie Infinity (AXS)", "color": "darkblue"},
+    "smooth-love-potion": {"token": "SLP", "name": "Smooth Love Potion (SLP)", "color": "hotpink"},
     "ronin": {"token": "RON", "name": "Ronin (RON)", "color": "blue"},
-    "solana": {"token": "SOL", "name": "Solana (SOL)", "color": "lime"},
+    "solana": {"token": "SOL", "name": "Solana (SOL)", "color": "purple"},
     "myria": {"token": "MYRIA", "name": "Myria (MYRIA)", "color": "navy"},
-    "chainlink": {"token": "LINK", "name": "Chainlink (LINK)", "color": "cyan"},
+    "sui": {"token": "SUI", "name": "Sui (SUI)", "color": "cyan"},
+    "kaspa": {"token": "KAS", "name": "Kaspa (KAS)", "color": "mediumseagreen"},
+    "virtual-protocol": {"token": "VIRTUAL", "name": "Virtuals Protocol (VIRTUAL)", "color": "green"},
+    "gold8": {"token": "GOLD8", "name": "Gold8 (GOLD8)", "color": "gold"},
+    "ai16z": {"token": "AI16Z", "name": "ai16z (AI16Z)", "color": "red"},
 }
 
 token_pairs = [
     ('bitcoin','ethereum'),
-    ('axie-infinity','ronin')
+    ('axie-infinity','ronin'),
+    ('axie-infinity','smooth-love-potion'),
+    ('virtual-protocol','ai16z'),
 ]
 
 
 # function to get the pandas dataframe with prices of a ticker per day, for the selected previous days
 def get_market_chart(ticker, currency='eur', days='30', interval='daily'):
+    print(ticker)
     url = f'https://api.coingecko.com/api/v3/coins/{ticker}/market_chart?vs_currency={currency}&days={days}&interval={interval}'
     headers = { "x-cg-demo-api-key": api_key }
     response = requests.get(url, headers=headers)
@@ -53,8 +60,8 @@ def get_ohlc(ticker, currency='eur', days='30'):
 
 
 # function generate a 2 lines plot to compare 2 tickers and save it to disk as a png image
-def generate_2_line_plot(x_series1, y_series1, color1, label1, ylabel1, x_series2, y_series2, color2, label2, ylabel2, plot_path):
-    fig, ax = plt.subplots(figsize=(4.5, 4))
+def generate_2_line_plot(x_series1, y_series1, color1, label1, ylabel1, x_series2, y_series2, color2, label2, ylabel2, figsize=(6, 4), plot_path):
+    fig, ax = plt.subplots(figsize=figsize)
     ax.tick_params(axis='x', labelsize=8, rotation=30)
     ax.plot(x_series1, y_series1, color=color1, label=label1)
     ax.set_ylabel(ylabel1)
@@ -69,8 +76,8 @@ def generate_2_line_plot(x_series1, y_series1, color1, label1, ylabel1, x_series
 
 
 # function generate a lines plot to and save it to disk as a png image
-def generate_1_line_plot(x_series, y_series, color, ylabel, plot_path):
-    plt.figure(figsize=(4.5, 4))
+def generate_1_line_plot(x_series, y_series, color, ylabel, figsize=(6, 4), plot_path):
+    plt.figure(figsize=figsize)
     plt.tick_params(axis='x', labelsize=8, rotation=30)
     plt.plot(x_series, y_series, color=color)
     plt.ylabel(ylabel)
@@ -106,12 +113,12 @@ def main():
         tickers_info.append({
             "ticker": '',
             "figure_path": figure_path,
-            "title": tickers_to_watch[ticker1]['name'] + " vs. " + tickers_to_watch[ticker2]['name'] + " Chart"
+            "title": tickers_to_watch[ticker1]['name'] + " and " + tickers_to_watch[ticker2]['name'] + " Chart"
             })
 
     # generate charts for the single line plots
     for ticker in tickers_to_watch:
-        if ticker in ['bitcoin','ethereum','axie-infinity','ronin']:
+        if ticker in ['bitcoin','ethereum','axie-infinity','ronin','smooth-love-potion','virtual-protocol','ai16z']:
             continue
         data = get_market_chart(ticker, days=days)
         figure_path = charts_path + tickers_to_watch[ticker]['token'] + '.png'
